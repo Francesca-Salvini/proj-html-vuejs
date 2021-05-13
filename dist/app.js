5403e11de273a23204909dad626cc553c093a7df
Francesca Salvini <franci98sp@gmail.com>
2021-05-13 14:42:47 +0200

    completata prima sezione footer

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,17 @@
 var app = new Vue({
   el: '#root',
   data: {
-    navbar: ['Home', 'Apple', 'Microsoft', 'Android', 'Forums', 'Contact us']
+    navbar: ['Home', 'Apple', 'Microsoft', 'Android', 'Forums', 'Contact us'],
+    footer: [{
+      h6: 'popular topics',
+      a: ['Nam a dolor volutpat massa auctor semper', 'Sed euismod nunc urna fermentum arcu dapibus fringilla.', 'Cras lacinia tellus id mauris finibus lacus molestie', 'Curabitur a scelerisque ligula']
+    }, {
+      h6: 'recent topics',
+      a: ['Cras lacinia tellus id mauris finibus lacus molestie', 'Proin at ligula sagittis vestibulum nisi vitae', 'Sed nec metus at est tincidunt elementum', 'Nulla egestas nulla eu nulla suscipit molestie']
+    }, {
+      h6: 'latest replies',
+      a: ['Cras lacinia tellus id mauris finibus lacus molestie', 'Cras lacinia tellus id mauris finibus lacus molestie', 'Nulla egestas nulla eu nulla suscipit molestie', 'Mauris ac nibh quis eros sagittis lacinia a et dui']
+    }]
   },
   methods: {}
 });
